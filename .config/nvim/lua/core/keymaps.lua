930892d5e2d2dd709dd8159638dd975392a0b38c
Julian Murphy <njmdistrisoft@gmail.com>
2023-10-04 20:31:20 -0300

    add indent

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -4,16 +4,16 @@ vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 
-vim.keymap.set('n', '<leader>nt', ':NERDTreeFind<CR>')
+-- vim.keymap.set('n', '<leader>nt', ':NERDTreeFind<CR>') not used
 -- save
 vim.keymap.set('n', '<leader>w', ':w<CR>')
 vim.keymap.set('n', '<leader>q', ':q<CR>')
 
 -- fast access fzf.vim search
-vim.keymap.set('n', '<leader>f', ':Files<CR>')
-vim.keymap.set('n', '<leader>b', ':Buffers<CR>')
-vim.keymap.set('n', '<leader>ag', ':Ag<CR>')
-vim.keymap.set('n', '<leader>fb', ':History<CR>')
+-- vim.keymap.set('n', '<leader>f', ':Files<CR>')
+-- vim.keymap.set('n', '<leader>b', ':Buffers<CR>')
+-- vim.keymap.set('n', '<leader>ag', ':Ag<CR>')
+-- vim.keymap.set('n', '<leader>fb', ':History<CR>')
 
 
 --- search easymotion temp. disabled
--- .config/nvim/lua/core/plugin_config/treesitter.lua
+++ .config/nvim/lua/core/plugin_config/treesitter.lua
@@ -2,6 +2,10 @@ require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
   ensure_installed = { "c", "lua", "rust", "ruby", "vim", "php", "html" },
 
+  indent = {
+      enable = true,
+      -- disable = { 'php' },
+  },
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
   auto_install = true,
